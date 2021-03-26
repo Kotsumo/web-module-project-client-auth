8c23c590f9b312a3ce880a9a69b3355457670a33
David <davidellis1020@gmail.com>
2021-03-25 22:48:33 -0700

    Added form to add new friend

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,8 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import Login from './components/Login';
 import PrivateRoute from "./components/PrivateRoute";
 import FriendsList from './components/FriendsList';
+import AddFriendForm from './components/AddFriendForm';
 
 import './App.css';
 
@@ -10,8 +11,13 @@ function App() {
     <Router>
     <div className="App">
       <h1>Hello friends!</h1>
-
+      <Link to="/friends/add-new">Add Friend</Link>
+      <Link to="/friends" style={{marginRight: "16px"}}>
+        See Friends
+      </Link>
+      
       <Switch>
+        <PrivateRoute path="/friends/add-new" component={AddFriendForm} />
         <PrivateRoute path="/friends" component={FriendsList} />
 
         <Route path='/login'>
