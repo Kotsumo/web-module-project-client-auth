b29535c5f8aa0d8a03f632a692e6e04aa4f8598a
David <davidellis1020@gmail.com>
2021-03-25 22:22:04 -0700

    Created a login form, created a friendslist page

---
+++ friends/src/App.js
@@ -0,0 +1,30 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Login from './components/Login';
+import PrivateRoute from "./components/PrivateRoute";
+import FriendsList from './components/FriendsList';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <h1>Hello friends!</h1>
+
+      <Switch>
+        <PrivateRoute path="/friends" component={FriendsList} />
+
+        <Route path='/login'>
+          <Login />
+        </Route>
+
+        <Route >
+          <Login />
+        </Route>
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
